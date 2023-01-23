1ae8ae2cec44a2d311788e3dcb2645a7b09f49a6
Pratik1603 <pratikgupta1603@gmail.com>
2023-01-23 19:32:59 +0530

    day15

---
+++ google/day15/maxConsecutiveFloor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        sort(special.begin(), special.end());
+        int ans = max(special[0]-bottom, top-special.back());
+        for(int i=1; i<special.size(); i++){
+           ans = max(ans, special[i]-special[i-1]-1);
+        } 
+        return ans;
+    }
+};
+
